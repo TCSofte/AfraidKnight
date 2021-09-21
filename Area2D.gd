7158887b49396d8ef13ef98820e8b9822ca8fc1e
Il Capitano <ctsmgames@gmail.com>
2021-09-22 01:18:38 +0200

    aa
    aa

--- Area2D.gd
+++ Area2D.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 
 # Declare member variables here. Examples:
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,9 +17,7 @@ func _ready():
 
 
 func _on_Area2D_area_entered(area):
-	if area.is_in_group("player"):
+	if area.is_in_group("Player"):
 		print("ccbccbcvbcvbcbvcbbvb")
-		$CanvasModulate.visible = true
-		$knight/Light2D.visible = true
-
-
+		$"../CanvasModulate".visible = true
+		$"../knight/Light2D".visible = true
